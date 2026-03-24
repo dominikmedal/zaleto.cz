88fd46b6b3a6b7ec9fc012baa57a0d46f0399323
Dominik Medal <web@dominikmedal.cz>
2026-03-24 18:45:04 +0100

    fixed bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,8 @@ const rateLimit = require('express-rate-limit')
 const app = express()
 const PORT = process.env.PORT || 3001
 
+app.set('trust proxy', 1)
+
 app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }))
 app.use(compression())
 app.use(express.json())
